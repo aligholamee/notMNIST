c326f95cc2d3001f060e9d720ffa7f74f377b20c
Ali Gholami <aligholami7596@gmail.com>
2018-01-24 15:40:23 +0330

    Fixed all pylint errors

--- src/fully-connected-network/model.py
+++ src/fully-connected-network/model.py
@@ -1,5 +1,3 @@
-# Training and Validation on notMNIST Dataset
-# Fully connected network implementation with tensorflow
 # ========================================
 # [] File Name : model.py
 #
@@ -7,18 +5,23 @@
 #
 # [] Created By : Ali Gholami (aligholami7596@gmail.com)
 # ========================================
+
+"""
+    Training and Validation on notMNIST Dataset
+    Fully connected network implementation with tensorflow
+"""
 import pickle as pickle
-import numpy as np 
+import numpy as np
 import tensorflow as tf
 
 # Data destination path
-pickle_file = "../../data/notMNIST.pickle"
+PICKLE_FILE = "../../data/notMNIST.pickle"
 
 # Load the data to the RAM
-with open(pickle_file, "rb") as f:
+with open(PICKLE_FILE, "rb") as f:
     SAVE_FILE = pickle.load(f)
 
-    TRAIN_DATASET = SAVE_FILE['train_dataset'] 
+    TRAIN_DATASET = SAVE_FILE['train_dataset']
     TRAIN_LABELS = SAVE_FILE['train_labels']
 
     VALID_DATASET = SAVE_FILE['valid_dataset']
@@ -32,25 +35,28 @@ with open(pickle_file, "rb") as f:
 
 # Reformat to the one-hot encoding mode
 # def reformatData(dataset, labels):
-image_size = 28
-num_labels = 10
+IMAGE_SIZE = 28
+NUM_LABELS = 10
+
 def reformat(dataset, labels):
-    n_dataset = dataset.reshape((1, image_size * image_size)).astype(np.float)
+    """
+        Reformat data to the one-hot and flattened mode
+    """
+    n_dataset = dataset.reshape((1, IMAGE_SIZE * IMAGE_SIZE)).astype(np.float)
 
     # Convert to the one hot format
-    n_labels = (np.arange(num_labels) == labels[:, None]).astype(np.float)
+    n_labels = (np.arange(NUM_LABELS) == labels[:, None]).astype(np.float)
 
     return n_dataset, n_labels
 
-TRAIN_DATASET, TRAIN_LABELS =  reformat(TRAIN_DATASET, TRAIN_LABELS) 
+TRAIN_DATASET, TRAIN_LABELS = reformat(TRAIN_DATASET, TRAIN_LABELS)
 VALID_DATASET, VALID_LABELS = reformat(VALID_DATASET, VALID_LABELS)
 TEST_DATASET, TEST_LABELS = reformat(TEST_DATASET, TEST_LABELS)
 
-# Display the openend files 
+# Display the openend files
 print("Training Set ", TRAIN_DATASET.shape, TRAIN_LABELS.shape)
 print("Validation Set", VALID_DATASET.shape, VALID_LABELS.shape)
 print("Test Set", TEST_DATASET.shape, TEST_LABELS.shape)
 
 # Implements a gradient descent using tensorflow computational graph
-train_subset = 10000
-
+TRAIN_SUBSET = 10000
