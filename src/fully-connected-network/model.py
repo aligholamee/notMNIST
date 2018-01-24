c369a11e347fbe0a000fe590a268251407761ba5
Ali Gholami <aligholami7596@gmail.com>
2018-01-24 14:36:25 +0330

    Data loading was completed

--- src/fully-connected-network/model.py
+++ src/fully-connected-network/model.py
@@ -7,3 +7,37 @@
 #
 # [] Created By : Ali Gholami (aligholami7596@gmail.com)
 # ========================================
+import pickle as pickle
+import numpy as np 
+import tensorflow as tf
+
+# Data destination path
+pickle_file = "../../data/notMNIST.pickle"
+
+# Load the data to the RAM
+with open(pickle_file, "rb") as f:
+    save = pickle.load(f)
+
+    train_dataset = save['train_dataset'] 
+    train_labels = save['train_labels']
+
+    valid_dataset = save['valid_dataset']
+    valid_labels = save['valid_labels']
+
+    test_dataset = save['test_dataset']
+    test_labels = save['test_labels']
+
+    # Free some memory
+    del save
+
+    # Display the openend files 
+    print("Training Set ", train_dataset.shape, train_labels.shape)
+    print("Validation Set", valid_dataset.shape, valid_labels.shape)
+    print("Test Set", test_dataset.shape, test_labels.shape)
+
+
+# Reformat to the one-hot encoding mode
+# def reformatData(dataset, labels):
+
+
+    
